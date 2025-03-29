c6f0580a563875cfbbb9ed3fdf8b42589a6aab87
yefrin1628 <98777340+yefrin1628@users.noreply.github.com>
2025-03-29 16:37:13 -0500

    Add app/src/main/java/com/example/simuladorventas/App.kt 1743284231992

--- app/src/main/java/com/example/simuladorventas/App.kt
+++ app/src/main/java/com/example/simuladorventas/App.kt
@@ -1,25 +1,16 @@
 package com.example.simuladorventas
 
 import android.app.Application
-import com.example.simuladorventas.di.AppModule
-import org.koin.android.ext.koin.androidContext
-import org.koin.core.context.startKoin
+import dagger.hilt.android.HiltAndroidApp
 
 /**
  * Clase principal de la aplicación que maneja la configuración inicial.
- * Configura Koin para la inyección de dependencias y realiza otras inicializaciones.
- * 
- * @property applicationContext Contexto global de la aplicación
- * @method onCreate() Se ejecuta al iniciar la aplicación, configura Koin DI
+ * HiltAndroidApp habilita la inyección de dependencias con Hilt.
  */
+@HiltAndroidApp
 class App : Application() {
     override fun onCreate() {
         super.onCreate()
-        
-        // Configuración de Koin para Dependency Injection
-        startKoin {
-            androidContext(this@App)
-            modules(AppModule.module)
-        }
+        // Configuración adicional si es necesaria
     }
 }
