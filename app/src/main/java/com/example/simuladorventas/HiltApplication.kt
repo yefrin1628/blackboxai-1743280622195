acbefbc7ce3b453adea63cf6e39a03eea681d418
yefrin1628 <98777340+yefrin1628@users.noreply.github.com>
2025-03-29 15:37:16 -0500

    Add app/src/main/java/com/example/simuladorventas/HiltApplication.kt 1743280635649

---
+++ app/src/main/java/com/example/simuladorventas/HiltApplication.kt
@@ -0,0 +1,12 @@
+package com.example.simuladorventas
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class HiltApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        // Initialization code if needed
+    }
+}
